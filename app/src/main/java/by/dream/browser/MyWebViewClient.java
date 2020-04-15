30b388c6c88f26866fe7940369744d4a750d9b20
dream-by <dzmitry.dzianisau@gmail.com>
2020-04-15 15:07:16 +0300

    universal method for browser

--- app/src/main/java/by/dream/browser/MyWebViewClient.java
+++ app/src/main/java/by/dream/browser/MyWebViewClient.java
@@ -1,6 +1,8 @@
 package by.dream.browser;
 
 import android.annotation.TargetApi;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Build;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
@@ -9,15 +11,16 @@ import android.webkit.WebViewClient;
 import javax.sql.RowSet;
 
 public class MyWebViewClient extends WebViewClient {
-    @TargetApi(Build.VERSION_CODES.N)
+
     @Override
-    public boolean shouldOverrideUrlLoading (WebView view, WebResourceRequest request){
+    public boolean shouldOverrideUrlLoading (WebView view, String url){
 
-        view.loadUrl(request.getUrl().toString());
-        return true;
+    if (Uri.parse(url).getHost().length()==0){
+            return false;
+        }
 
-
-
+    Intent intent = new Intent (Intent.ACTION_VIEW,Uri.parse(url));
+    view.getContext().startActivity(intent);
+    return true;
     }
-
 }
